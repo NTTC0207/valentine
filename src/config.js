faeecbc686ae6768aae323b1e0ebbb7d5faedafc
Hao Yang <haoyang@tsunago.asia>
2026-02-14 14:42:11 +0800

    update

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@
 export const APP_CONFIG = {
   // Set to true to block inspection and right-click
   // Set to false to allow developer tools (for debugging)
-  ENABLE_DEV_TOOLS_BLOCKER: false,
+  ENABLE_DEV_TOOLS_BLOCKER: true,
 };
